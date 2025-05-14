f88e1a2604bfb20b883756b837e91bc88b3b254c
Michael Andorful <andorfulmichaelebo@gmail.com>
2025-05-14 23:45:04 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+  <h1 className="text-3xl font-bold text-blue-600">
+        React + TypeScript + Tailwind CSS + Vite
+      </h1>
+      
+    </div>
+  )
+}
+
+export default App
